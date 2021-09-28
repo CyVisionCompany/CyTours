56c69b3261875f3d12fd78ca16ddb4812461d85e
hasti-davami <89081164+hasti-davami@users.noreply.github.com>
2021-09-28 15:16:27 +0330

    fix navbar _ discount tours _ top tours

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,8 +1,11 @@
 'use strict'
 const menu = document.querySelector('.header__menu-box');
 const hamburger = document.getElementById('hamburger-btn');
+const navbar = document.querySelector('#nav');
 hamburger.addEventListener('click',function(){
 
+        
         menu.classList.toggle('activee');
         hamburger.classList.toggle('check');
+        navbar.classList.toggle("fix");
 });
